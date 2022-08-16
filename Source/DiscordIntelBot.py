0b3c73a4c405572bc3b0827be74c34d08b59ab1e
jon <43426006+plawnoj@users.noreply.github.com>
2022-08-16 19:07:26 -0400

    push

--- Source/DiscordIntelBot.py
+++ Source/DiscordIntelBot.py
@@ -5,7 +5,7 @@ import requests
 from discord import Webhook, RequestsWebhookAdapter
 import urllib.request, json
 
-ConfigurationFilePath = "C:\\Users\\User\\Documents\\config.txt" ##path to configuration file
+ConfigurationFilePath = "C:\\Users\\User\\Documents\\ThreatIntelligenceDiscordBot\\Source\\Config.txt" ##path to configuration file
 
 
 FileConfig = ConfigParser()
